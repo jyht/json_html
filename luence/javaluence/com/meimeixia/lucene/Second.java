23068a4b87d90fb349ea5122b4310943110163a6
jyht <304931037@qq.com>
2020-03-25 20:10:28 +0800

    ik.jar 没用系统的好像就是ik
    ik.jar 没用系统的好像就是ik

--- luence/javaluence/com/meimeixia/lucene/Second.java
+++ luence/javaluence/com/meimeixia/lucene/Second.java
@@ -25,17 +25,17 @@ public class Second {
         IndexReader reader = DirectoryReader.open(dir);
         IndexSearcher is = new IndexSearcher(reader);
         QueryParser parser = new QueryParser("title", a);
-        Query query = parser.parse("姐姐");
-        TopDocs topDocs = is.search(query, 5);
+        Query query = parser.parse("补习");
+        TopDocs topDocs = is.search(query, 10);
         //System.out.println("总共匹配多少个：" + topDocs.totalHits);
         ScoreDoc[] hits = topDocs.scoreDocs;
         // 应该与topDocs.totalHits相同
-        //System.out.println("多少条数据：" + hits.length);
+        System.out.println("多少条数据：" + hits.length);
         for (ScoreDoc scoreDoc : hits) {
-            //System.out.println("匹配得分：" + scoreDoc.score);
+            System.out.println("匹配得分：" + scoreDoc.score);
             //System.out.println("文档索引ID：" + scoreDoc.doc);
             Document document = is.doc(scoreDoc.doc);
-            System.out.println(document.get("id")+"-1--"+document.get("title"));
+            System.out.println(document.get("id")+"--"+document.get("title"));
         }
         reader.close();
         dir.close();
