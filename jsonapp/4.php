91639c131ed16cc0d2a1313fcaa3a3a36817f990
jyht <304931037@qq.com>
2020-04-08 07:24:46 +0800

    搬家
    1

--- jsonapp/4.php
+++ jsonapp/4.php
@@ -1,10 +1,13 @@
 <?php
     
 function demos(){
+	
+	
+$array = array("Eric",23);
         $data=[
             'status'=>200,
-            'msg'=>'ok'
-			'body'
+            'msg'=>'ok',
+			'body'=>$array
         ];
         return json_encode($data);
     }
