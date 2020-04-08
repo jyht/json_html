a5ab14402fc3f22ba5aecc9b2c2587f7d7355d60
jyht <304931037@qq.com>
2020-04-08 12:38:45 +0800

    Update getPostList.php

--- jsonapp/getPostList.php
+++ jsonapp/getPostList.php
@@ -22,10 +22,14 @@ while($row = mysqli_fetch_array($dsa))
 	//$string = "as</br>12村已堵，\n路已封，\n为了安全躲家中。[傲慢]\n新肺炎，\n扩散快，\n严防死守这一带。[拳头]\n一人染，\n全家病，\n严重起来真要命！";
 	//24行 读取数据库字段的就无法解析 json 会转义变成\\n
 	$string = $str=stripcslashes($row['content']);  //  \\n新肺炎，\\n扩散
-	$as = array('content'=>array('title'=>"$row[title]",'content'=>"$string"));
-	echo json_encode($as,JSON_UNESCAPED_UNICODE);die();
-	$arr[] = array('pid'=>''.$row['pid'].'','uid'=>''.$row['uid'].'','content'=>array('title'=>"$row[title]",'content'=>"$string"),'postDate'=>''.$row['postDate'].'','readCnt'=>''.$row['readCnt'].'','praiseCnt'=>''.$row['praiseCnt'].'','images'=>''.$row['images'].'','type'=>''.$row['type'].'','status'=>''.$row['status'].'','imageNum'=>''.$row['imageNum'].'','user'=>array('id'=>''.$row['id'].'','user_id'=>''.$row['user_id'].'','name'=>''.$row['name'].'','nickName'=>''.$row['nickName'].'','sex'=>''.$row['sex'].'','location'=>''.$row['location'].'','age'=>''.$row['age'].'','register_time'=>''.$row['register_time'].'','avatar'=>''.$row['avatar'].'','avatar_thumb'=>''.$row['avatar_thumb'].'','notes'=>''.$row['notes'].'','score'=>''.$row['score'].'','fans'=>''.$row['fans'].'','praise'=>''.$row['praise'].'','last_login_time'=>''.$row['last_login_time'].'','status'=>''.$row['status'].'','bornDate'=>''.$row['bornDate'].'','lastLaunchDate'=>''.$row['lastLaunchDate'].'','zanCnt'=>''.$row['zanCnt'].'','fanCnt'=>''.$row['fanCnt'].''));
+	$zhyyao =array('title'=>'das','content'=>'dsadsa');
+	//$zhyyao = addslashes($zhyyao);
+	$asd = json_encode($zhyyao);
+	$arr = array('pid'=>''.$row['pid'],'content'=>$asd);
 }
+
+
+
 
 
 $array = array("Eric",23);
