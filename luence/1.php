179b9cea817073cdb5c3a8ce467d72cf49968c76
jyht <304931037@qq.com>
2020-04-23 16:35:52 +0800

    1
    1

--- luence/1.php
+++ luence/1.php
@@ -6,7 +6,7 @@ java_set_file_encoding("UTF-8"); //设置JAVA编码。
 
 $test=new Java("com.meimeixia.lucene.jyht");
 
-$ass = json_decode($test->select("开如苞艳史"));
+$ass = json_decode($test->select("古墓"));
 var_dump($ass);
 
 //$str=json_decode($arr);
