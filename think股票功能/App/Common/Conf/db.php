2d2127348ef5a3acf357bd4d98db9a31f0d54dba
jyht <304931037@qq.com>
2023-10-13 13:43:33 +0800

    股票thinkphp
    股票thinkphp 5.4以上php版本

---
+++ think股票功能/App/Common/Conf/db.php
@@ -0,0 +1,12 @@
+<?php return array (
+  'DB_TYPE' => 'mysql',
+
+  'DB_HOST' => 'localhost',
+  'DB_NAME' => '52jscn',
+  'DB_USER' => 'root',
+  'DB_PWD' => '',
+  'DB_PORT' => '3306',
+  'DB_PREFIX' => 'yang_',
+  'DB_FIELDS_CACHE' => true,
+  'DB_CHARSET' => 'utf8',
+);
